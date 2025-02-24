46e8971467679197f751b6fc043d59e2752866ce
ruidong <ruidong@mit.edu>
2025-02-24 12:14:45 -0500

    Initial Commit

---
+++ crosshair/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  crosshair
+//
+//  Created by Ruidong Chen on 2/24/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
